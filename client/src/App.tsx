10169ea79dc8565b1b0ca9261347377de2815d3d
Inobera <inobera2019@gmail.com>
2026-04-17 11:21:00 +0300

    feat: initialize App component with wouter routing, providers, and base path support

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,40 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Route, Switch, Router as WouterRouter } from "wouter"; // Importuok Router
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 import { LanguageProvider } from "./contexts/LanguageContext";
+
 function Router() {
-  // make sure to consider if you need authentication for certain routes
   return (
     <Switch>
-      <Route path={"/"} component={Home} />
-      <Route path={"/404"} component={NotFound} />
+      <Route path="/" component={Home} />
+      {/* Naudojant base, šie keliai bus santykiniai, t.y. /gustra/404 */}
+      <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
     </Switch>
   );
 }
 
 function App() {
+  // Naudojame Vite aplinkos kintamąjį, kuris GitHub'e bus "/gustra"
+  const base = import.meta.env.BASE_URL.replace(/\/$/, "");
+
   return (
     <ErrorBoundary>
-      <ThemeProvider defaultTheme="light">
-        <LanguageProvider>
-          <TooltipProvider>
-            <Toaster />
-            <Router />
-          </TooltipProvider>
-        </LanguageProvider>
-      </ThemeProvider>
+      {/* Apgaubiame viską su Wouter Router ir nurodome base */}
+      <WouterRouter base={base}>
+        <ThemeProvider defaultTheme="light">
+          <LanguageProvider>
+            <TooltipProvider>
+              <Toaster />
+              <Router />
+            </TooltipProvider>
+          </LanguageProvider>
+        </ThemeProvider>
+      </WouterRouter>
     </ErrorBoundary>
   );
 }
